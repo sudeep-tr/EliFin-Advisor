4139a6c0508f4721b252c843bf733de20497bf35
sudeep <sudeep.trics@gmail.com>
2026-08-21 20:19:36 +0530

    Add JWT Key For Better Security

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -7,32 +7,44 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
-    const { name, email, password, income, age} = req.body;
+    const { name, email, password, age, income } = req.body;
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({
-        message: "User already exists",
+        message: "User already exists"
       });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       age,
       income
     });
 
     res.status(201).json({
       message: "User registered successfully",
-      user,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        age: user.age,
+        income: user.income,
+        role: user.role
+      }
     });
+
   } catch (error) {
+    console.error("REGISTER ERROR:", error);
+
     res.status(500).json({
       message: "Server error",
-      error: error.message,
+      error: error.message
     });
   }
 });
@@ -48,15 +60,39 @@ router.post("/login", async (req, res) => {
       });
     }
 
-    if (user.password !== password) {
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
       return res.status(400).json({
         message: "Invalid email or password"
       });
     }
 
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        role: user.role
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
     res.status(200).json({
       message: "Login successful",
-      user
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        age: user.age,
+        income: user.income,
+        role: user.role
+      }
     });
 
   } catch (error) {
