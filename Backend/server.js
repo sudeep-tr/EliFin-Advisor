491f47b2be639be61d553e1a2b5cdfaef29e05c4
ghanavaibhav08-sys <ghanavaibhav08@gmail.com>
2026-08-22 14:30:46 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,8 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: [ 
+      "http://localhost:5173",
+      "https://your-frontend.vercel.app",
+    ],
+      credentials: true,
   })
 );
 
